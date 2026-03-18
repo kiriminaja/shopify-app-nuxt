c8017d25db8491b92f2d2ae7ccd4b78f3a1338c1
Yanuar Aditia <yanuaraditia@outlook.com>
2026-03-18 19:04:26 +0700

    feat: update project preparation

---
+++ eslint.config.ts
@@ -0,0 +1,32 @@
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+export default createConfigForNuxt({
+  features: {
+    tooling: true,
+    stylistic: {
+      commaDangle: 'never',
+      braceStyle: '1tbs'
+    }
+  }
+}).overrideRules({
+  'import/first': 'off',
+  'import/order': 'off',
+  'vue/multi-word-component-names': 'off',
+  'vue/max-attributes-per-line': ['error', { singleline: 5 }],
+  '@typescript-eslint/ban-types': 'off',
+  '@typescript-eslint/no-empty-object-type': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
+  '@stylistic/comma-dangle': 'off',
+  '@stylistic/quotes': 'off',
+  '@stylistic/quote-props': 'off',
+  '@typescript-eslint/ban-ts-comment': 'warn',
+  '@stylistic/operator-linebreak': 'off',
+  '@stylistic/arrow-parens': 'off',
+  '@stylistic/indent-binary-ops': 'off',
+  'vue/singleline-html-element-content-newline': 'off',
+  'vue/operator-linebreak': 'off',
+  '@stylistic/member-delimiter-style': 'off',
+  '@stylistic/indent': 'off',
+  'vue/html-indent': 'off',
+  'vue/html-self-closing': 'off'
+})
