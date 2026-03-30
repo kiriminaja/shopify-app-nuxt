584cb982e0545a6bd81852f9d43b27e3c23b1240
Yanuar Aditia <yanuaraditia@outlook.com>
2026-03-30 15:46:14 +0700

    feat: add ShLoadingIndicator component to manage loading state in Shopify app

---
+++ src/runtime/components/ShLoadingIndicator.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useLoadingIndicator, useAppBridge } from '#imports'
+import { watch, onBeforeUnmount } from 'vue'
+
+const { isLoading } = useLoadingIndicator()
+const shopify = useAppBridge()
+
+watch(isLoading, (loading) => {
+  shopify.loading(loading)
+})
+
+onBeforeUnmount(() => {
+  shopify.loading(false)
+})
+</script>
+
+<template>
+  <slot />
+</template>
