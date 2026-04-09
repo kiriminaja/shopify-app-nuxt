54e38038389fae26280621c99a1c4ac6a3e2f622
Yanuar Aditia <yanuaraditia@outlook.com>
2026-04-09 14:15:37 +0700

    feat: refactor Banner component to improve event handling and update tone type definition

--- src/runtime/components/polaris/Banner.vue
+++ src/runtime/components/polaris/Banner.vue
@@ -1,20 +1,29 @@
 <template>
-  <s-banner v-bind="polarisAttrs">
+  <s-banner
+    v-bind="polarisAttrs"
+    @dismiss="emit('dismiss', $event)"
+    @after-hide="emit('afterHide', $event)"
+  >
     <slot />
   </s-banner>
 </template>
 
 <script setup lang="ts">
+import type { ToneKeyword } from '../../types'
 import { usePolarisAttrs } from './utils'
 
 defineOptions({ inheritAttrs: false })
 
 const props = defineProps<{
   heading?: string
-  tone?: 'info' | 'success' | 'warning' | 'critical' | 'auto'
-  hidden?: boolean
+  tone?: ToneKeyword
   dismissible?: boolean
+  hidden?: boolean
 }>()
 
 const polarisAttrs = usePolarisAttrs(props)
+
+const emit = defineEmits<{
+  (e: 'dismiss' | 'afterHide', event: Event): void
+}>()
 </script>
